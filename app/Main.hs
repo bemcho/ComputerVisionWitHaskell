5bdf636be83ba2e90e7d2b6068b3046913696931
Emil Tomov <bemcho@gmail.com>
2020-03-30 19:08:03 +0300

    Open file dialog - done

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,8 @@
 module Main where
 
 import           Control.Monad
-import           Graphics.UI.Gtk hiding (Action, backspace)
 import           UI.Utils
 
 main :: IO ()
 main = runApp
-  
+
--- src/UI/Utils.hs
+++ src/UI/Utils.hs
@@ -6,23 +6,23 @@ module UI.Utils
   ) where
 
 import           Control.Monad.IO.Class
+import           Data.Maybe
+import qualified Data.Text               as T
 import           Graphics.UI.Gtk         hiding (Action, backspace)
 import           Graphics.UI.Gtk.Builder
 
-runApp::IO()
+runApp :: IO ()
 runApp = do
   initGUI -- (1)
   --todo embed .glade file in executable and use relative path
   builder <-
     initUIBuilder "/home/bemcho/Projects/HaskellWorld/CVWithHaskell/resource/ui/ComputerVisionWitHaskell_UI.glade"
-  
   windowToolbar <- getToolbarWindow builder
   windowDrawingArea <- getDrawingAreaWindow builder
-  
   widgetShowAll windowToolbar
   widgetShowAll windowDrawingArea
   mainGUI
-  
+
 initUIBuilder :: String -> IO Builder
 initUIBuilder path = do
   builder <- builderNew
@@ -30,12 +30,65 @@ initUIBuilder path = do
   return builder
 
 getToolbarWindow :: Builder -> IO Window
-getToolbarWindow b = do 
-  window <- builderGetObject b castToWindow "main_window_toolbar"
+getToolbarWindow b = do
+  window <- getWindow b "main_window_toolbar"
   window `on` deleteEvent $ -- handler to run on window destruction
-          liftIO mainQuit >>
-          return False
+    liftIO mainQuit >>
+    return False
+  openFileBtn <- getButton b "open_image_btn"
+  openFileBtn `on` buttonActivated $ -- handler to run open image
+   do
+    maybePath <- runOpenImageDialog
+    putStrLn (fromMaybe "" maybePath)
+    return ()
   return window
 
 getDrawingAreaWindow :: Builder -> IO Window
-getDrawingAreaWindow b = builderGetObject b castToWindow "main_window_draw"
+getDrawingAreaWindow b = getWindow b "main_window_draw"
+
+getEditResizeWindow :: Builder -> IO Window
+getEditResizeWindow b = getWindow b "edit_resize_window"
+
+getEditCropWindow :: Builder -> IO Window
+getEditCropWindow b = getWindow b "edit_crop_window"
+
+getEditScaleWindow :: Builder -> IO Window
+getEditScaleWindow b = getWindow b "edit_scale_window"
+
+getWindow :: Builder -> String -> IO Window
+getWindow b id = builderGetObject b castToWindow id
+
+getButton :: Builder -> String -> IO Button
+getButton b id = builderGetObject b castToButton id
+
+getSliderAdjustment :: Builder -> String -> IO Adjustment
+getSliderAdjustment b id = builderGetObject b castToAdjustment id
+
+runOpenImageDialog :: IO (Maybe String)
+runOpenImageDialog = do
+  hsfilt <- fileFilterNew
+  fileFilterAddPattern hsfilt "*.png"
+  fileFilterAddPattern hsfilt "*.jpg"
+  fileFilterAddPattern hsfilt "*.tiff"
+  fileFilterAddPattern hsfilt "*.gif"
+  fileFilterSetName hsfilt "Image Files"   
+  
+  fchdal <-
+    fileChooserDialogNew
+      (Just "Open image Dialog")
+      Nothing
+      FileChooserActionOpen
+      [("Cancel", ResponseCancel), ("Open", ResponseAccept)]
+  fileChooserAddFilter fchdal hsfilt    
+  widgetShow fchdal
+  response <- dialogRun fchdal
+  case response of
+    otherwise -> do
+      widgetDestroy fchdal
+      return Nothing
+    ResponseAccept -> do
+      nwf <- fileChooserGetFilename fchdal
+      case nwf of
+        Just path -> do
+          widgetDestroy fchdal
+          return (Just path)
